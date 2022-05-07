5143d7eeec5a2799a1facbf34cb49aba1c311fb3
Alexandre <alee.wxa@gmail.com>
2022-05-06 22:45:13 -0300

    feat: adiciona seção 3 concluída

--- React-M.S/react-complete-guide/src/App.js
+++ React-M.S/react-complete-guide/src/App.js
@@ -1,12 +1,38 @@
-import ExpenseItem from "./components/ExpenseItem";
+import Expenses from "./components/Expenses/Expenses";
 
-function App() {
+const App = () => {
+    const expenses = [
+        {
+            ide: "e1",
+            title: "Toiled Paper",
+            amount: 94.12,
+            date: new Date(2022, 5, 5),
+        },
+        {
+            ide: "e2",
+            title: "New TV",
+            amount: 799.49,
+            date: new Date(2022, 5, 4),
+        },
+        {
+            ide: "e3",
+            title: "Car Insurance",
+            amount: 294.67,
+            date: new Date(2022, 5, 3),
+        },
+        {
+            ide: "e4",
+            title: "New Desk (Wooden)",
+            amount: 450,
+            date: new Date(2022, 5, 2),
+        },
+    ];
     return (
         <div>
             <h2>Let's get started!</h2>
-            <ExpenseItem />
+            <Expenses item={expenses} />
         </div>
     );
-}
+};
 
 export default App;
